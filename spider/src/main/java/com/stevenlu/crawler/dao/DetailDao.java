fad816db54ea09c6095c286a9d78fde9f6b080d5
StevenLu <gzstevenlu@gmail.com>
2016-05-17 21:12:53 +0800

    finish DetailDao

--- spider/src/main/java/com/stevenlu/crawler/dao/DetailDao.java
+++ spider/src/main/java/com/stevenlu/crawler/dao/DetailDao.java
@@ -11,7 +11,11 @@ import com.stevenlu.crawler.utils.DBUtil;
 
 public class DetailDao {
 	
-	private static final String ADD_DETAIL = "";
+	private static final String ADD_DETAIL = "INSERT INTO detail (name, href, "
+			+ "bio, weibo, location, bussiness, gender, employment, position, "
+			+ "description, asks, answers, posts, collections, vote, thank, "
+			+ "fav, shares) VALUES (?, ?, ?, ?, ?,  ?, ?, ?, ?, ?,  ?, ?, ?, ?, ?, "
+			+ "?, ?, ?)";
 	
 	public int batchAddDetail(BlockingQueue<Detail> queue) {
 		Connection conn = DBUtil.getConnection();
@@ -23,6 +27,27 @@ public class DetailDao {
 			Iterator<Detail> it = queue.iterator();
 			while (it.hasNext()) {
 				Detail detail = it.next();
+				pstmt.setString(1, detail.name);
+				pstmt.setString(2, detail.href);
+				pstmt.setString(3, detail.bio);
+				pstmt.setString(4, detail.weibo);
+				pstmt.setString(5, detail.location);
+				
+				pstmt.setString(6, detail.bussiness);
+				pstmt.setString(7, detail.gender);
+				pstmt.setString(8, detail.employment);
+				pstmt.setString(9, detail.position);
+				pstmt.setString(10, detail.description);
+				
+				pstmt.setInt(11, Integer.parseInt(detail.asks));
+				pstmt.setInt(12, Integer.parseInt(detail.answers));
+				pstmt.setInt(13, Integer.parseInt(detail.posts));
+				pstmt.setInt(14, Integer.parseInt(detail.collections));
+				pstmt.setInt(15, Integer.parseInt(detail.vote));
+				
+				pstmt.setInt(16, Integer.parseInt(detail.thank));
+				pstmt.setInt(17, Integer.parseInt(detail.fav));
+				pstmt.setInt(11, Integer.parseInt(detail.shares));
 				
 				pstmt.addBatch();
 			}
